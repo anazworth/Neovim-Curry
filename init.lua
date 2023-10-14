d42a1d74fa1831aa9f49055849056b38b13d846f
anazworth <98629900+anazworth@users.noreply.github.com>
2023-10-14 14:02:30 -0400

    update for lv 10

--- init.lua
+++ init.lua
@@ -1,2 +1,12 @@
+if (vim.g.vscode) then
+    -- keybind for vscode
+    -- set space as leader key
+    vim.g.mapleader = " "
+    -- toggle vscode terminal with <leader> t
+    vim.keymap.set("n", "<leader>t", "<cmd>call VSCodeNotifyVisual('workbench.action.terminal.toggleTerminal', 1)<CR>", {})
+    vim.keymap.set("n", "<leader>e", "<cmd>call VSCodeNotifyVisual('workbench.view.explorer', 1)<CR>", {})
+    vim.keymap.set("n", "<leader>ff", "<cmd>call VSCodeNotify('workbench.action.quickOpenNavigateNextInFilePickerr', 1)<CR>", {})
+else
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+    require("config.lazy")
+end
