870b046e17a065039d4d07661369fbbce1a24d39
Austin Nazworth <austin.nazworth@gmail.com>
2024-11-22 00:33:17 -0500

    Change dashboard to snacks.dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,85 +1,67 @@
 return {
-	"nvimdev/dashboard-nvim",
-	event = "VimEnter",
-	opts = function()
-		local logo = [[
- ██████╗██╗   ██╗██████╗ ██████╗ ██╗   ██╗██╗   ██╗██╗███╗   ███╗
-██╔════╝██║   ██║██╔══██╗██╔══██╗╚██╗ ██╔╝██║   ██║██║████╗ ████║
-██║     ██║   ██║██████╔╝██████╔╝ ╚████╔╝ ██║   ██║██║██╔████╔██║
-██║     ██║   ██║██╔══██╗██╔══██╗  ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║
-╚██████╗╚██████╔╝██║  ██║██║  ██║   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
- ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
-                                                                 
-
-                      °°.                                 
-                    °*oO#                                
-    "henlo dad"      *#*.##           .**                
-                    .oO*°OO##OO##o*oOoO#o                
-                      O##Oo**O@@##@##**o°          "such wow"   
-                      0@@@   @@@@@@   @o                  
-                    .@@@@@OoO@@@@@oO@o.                  
-                    o#@o.o###*   .#@O       "cheeze pls"    
-                    °**##  ..     *O°                     
-                    *o°°o#° oooOOO*°...                   
-                    ..°..°°.O@#O##O°O##Oo...°°..          
-                  o°         °o#@@O#@@@#OOo°*.           
-                  #o         O@@@@@@@#OOO##@@@*          
-                  ##*       O#@@@##**o.**O@##@*          
-                  O##O°     .O##OO   *Oo..o@#@o          
-                  O@@#...  .  .oo* °*O@*  ..O@O          
-                  O@@#         o*O** *@#  .. #@O         
-                  O@@°         *O#oo°#@°   °°*@#*        
-                  O@@o         oO#*O@@#O     @@@@        
-        ooOOO#####O@@@##@@@@@@@@O#O°###@@@##O#@#@@@@@@@@@#
-        oo*ooOOOOO@@@#OOOOOOOOOOO@**@###@@@@@@@@@@@@@@@@@@
-    ]]
-
-		logo = string.rep("\n", 8) .. logo .. "\n\n"
-
-		local opts = {
-			theme = "doom",
-			hide = {
-				-- this is taken care of by lualine
-				-- enabling this messes up the actual laststatus setting after loading a file
-				statusline = false,
-			},
-			config = {
-				header = vim.split(logo, "\n"),
+	"folke/snacks.nvim",
+	opts = {
+		dashboard = {
+			preset = {
+				header = [[
+        ██████╗██╗   ██╗██████╗ ██████╗ ██╗   ██╗██╗   ██╗██╗███╗   ███╗ 
+        ██╔════╝██║   ██║██╔══██╗██╔══██╗╚██╗ ██╔╝██║   ██║██║████╗ ████║
+        ██║     ██║   ██║██████╔╝██████╔╝ ╚████╔╝ ██║   ██║██║██╔████╔██║
+        ██║     ██║   ██║██╔══██╗██╔══██╗  ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║
+        ╚██████╗╚██████╔╝██║  ██║██║  ██║   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
+        ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝ 
+                                                                         
+                              °°.                                        
+                            °*oO#                                        
+            "henlo dad"      *#*.##           .**                        
+                            .oO*°OO##OO##o*oOoO#o                        
+                              O##Oo**O@@##@##**o°          "such wow"    
+                              0@@@   @@@@@@   @o                         
+                            .@@@@@OoO@@@@@oO@o.                          
+                            o#@o.o###*   .#@O       "cheeze pls"         
+                            °**##  ..     *O°                            
+                            *o°°o#° oooOOO*°...                          
+                            ..°..°°.O@#O##O°O##Oo...°°..                 
+                          o°         °o#@@O#@@@#OOo°*.                   
+                          #o         O@@@@@@@#OOO##@@@*                  
+                          ##*       O#@@@##**o.**O@##@*                  
+                          O##O°     .O##OO   *Oo..o@#@o                  
+                          O@@#...  .  .oo* °*O@*  ..O@O                  
+                          O@@#         o*O** *@#  .. #@O                 
+                          O@@°         *O#oo°#@°   °°*@#*                
+                          O@@o         oO#*O@@#O     @@@@                
+ ]],
         -- stylua: ignore
-        center = {
-          { action = "Telescope find_files",              desc = " Find file",       icon = " ", key = "f" },
-          { action = "ene | startinsert",                 desc = " New file",        icon = " ", key = "n" },
-          { action = "Telescope oldfiles",                desc = " Recent files",    icon = " ", key = "r" },
-          { action = "Telescope live_grep",               desc = " Find text",       icon = " ", key = "g" },
-          { action = "e $MYVIMRC",                        desc = " Config",          icon = " ", key = "c" },
-          { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-          { action = "LazyExtras",                        desc = " Lazy Extras",     icon = " ", key = "e" },
-          { action = "Lazy",                              desc = " Lazy",            icon = "󰒲 ", key = "l" },
-          { action = "qa",                                desc = " Quit",            icon = " ", key = "q" },
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
-				footer = function()
-					local stats = require("lazy").stats()
-					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-					return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-				end,
 			},
-		}
-
-		for _, button in ipairs(opts.config.center) do
-			button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-		end
-
-		-- close Lazy and re-open when the dashboard is ready
-		if vim.o.filetype == "lazy" then
-			vim.cmd.close()
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "DashboardLoaded",
-				callback = function()
-					require("lazy").show()
-				end,
-			})
-		end
-
-		return opts
-	end,
+			sections = {
+				{ section = "header" },
+				{
+					icon = " ",
+					title = "Recent Files",
+					section = "recent_files",
+					indent = 2,
+					padding = { 2, 6 },
+					pane = 2,
+				},
+				{
+					section = "keys",
+					pane = 2,
+					padding = 2,
+				},
+				{ section = "startup", pane = 2, padding = 2 },
+			},
+		},
+	},
 }
